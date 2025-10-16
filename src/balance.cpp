c087bad4a256d9d28b9b725f15c7db9455c630aa
Leo Xie <leoxie@LeodeMacBook-Air-9.local>
2025-10-16 14:19:00 -0400

    submit

--- src/balance.cpp
+++ src/balance.cpp
@@ -8,7 +8,22 @@ static bool is_balanced_rec(const std::string& s, int idx, int open) {
     // - If '(' => recurse open+1
     // - If ')' => recurse open-1
     // - else ignore
-    return false;
+    if (open < 0) {
+        return false;
+    }
+    if (idx == s.size()) {
+        return open == 0;
+    }
+
+    char current_char = s[idx];
+    int next_open = open; 
+    if (current_char == '(') {
+        next_open = open + 1;
+    } else if (current_char == ')') {
+
+        next_open = open - 1;
+    }
+    return is_balanced_rec(s, idx + 1, next_open);
 }
 
 bool is_balanced(const std::string& s) {
